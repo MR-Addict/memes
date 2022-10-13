ef1695050e06f19e23155eb4a4a6190ce9ad3a05
MR-Addict <2750417853@qq.com>
2022-10-13 20:12:50 +0800

    Initialize project using Create React App

---
+++ app.js
@@ -0,0 +1,16 @@
+const memes = {
+  postLink: "https://redd.it/y2izsv",
+  subreddit: "dankmemes",
+  title: "im so proud of this community",
+  url: "https://i.redd.it/p8h1ckytlgt91.jpg",
+  nsfw: false,
+  spoiler: false,
+  author: "DifferentLow4875",
+  ups: 16913,
+  preview: [
+    "https://preview.redd.it/p8h1ckytlgt91.jpg?width=108\u0026crop=smart\u0026auto=webp\u0026s=fcf8ccf31eefd5ca7adba8e3ca0cf02336a7c033",
+    "https://preview.redd.it/p8h1ckytlgt91.jpg?width=216\u0026crop=smart\u0026auto=webp\u0026s=42763de6e4dac44cba2f9db6232a5aedd2620289",
+    "https://preview.redd.it/p8h1ckytlgt91.jpg?width=320\u0026crop=smart\u0026auto=webp\u0026s=d7ff67901c5f9a35dec25bf4c7b081065156d0f9",
+    "https://preview.redd.it/p8h1ckytlgt91.jpg?width=640\u0026crop=smart\u0026auto=webp\u0026s=a8a4ac523d1bd0ab8348114ae0972e2275324d90",
+  ],
+};
